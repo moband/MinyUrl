593ca3ccda2c83cc13870217364b23b5452a5ce6
MohammadReza Alagheband <MohammadReza.Alagheband@tecnotree.com>
2018-05-12 17:13:33 +0430

    CachePut added to update the cache on each saving

--- MinyUrlService/src/main/java/com/neueda/kgs/repository/ShortUrlRepository.java
+++ MinyUrlService/src/main/java/com/neueda/kgs/repository/ShortUrlRepository.java
@@ -2,6 +2,7 @@ package com.neueda.kgs.repository;
 
 import com.neueda.kgs.config.CacheConfig;
 import com.neueda.kgs.model.ShortUrl;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,7 @@ public interface ShortUrlRepository extends MongoRepository<ShortUrl,String> {
     @Cacheable(value = CacheConfig.CACHE_MINYLY)
     ShortUrl findByKey(Long key);
     ShortUrl findByLongUrl(String url);
+
+    @CachePut(value = CacheConfig.CACHE_MINYLY,key = "#s.key")
+    ShortUrl save(ShortUrl s);
 }
